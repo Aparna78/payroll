0468ffeca45fe1097b9beb00c8300f68f4c7176b
Aparna Kumari <aparnakumari@Aparnas-MacBook-Air.local>
2021-06-25 13:39:48 +0530

    first commit

---
+++ src/main/java/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
